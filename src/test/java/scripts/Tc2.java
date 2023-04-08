3eae4077ab0daa859b06fb7d527aa6f0c72c63fe
satish dashyal <sdashyal.21@gmail.com>
2023-04-08 14:45:58 +0530

    Project

---
+++ src/test/java/scripts/Tc2.java
@@ -0,0 +1,12 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc2 {
+	@Test
+	public void Demo() {
+		Reporter.log("Hello Selenium",true);
+	}
+
+}
---
+++ src/test/java/scripts/Tc3.java
@@ -0,0 +1,12 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc3 {
+	@Test
+	public void Demo() {
+		Reporter.log("Hello Python",true);
+	}
+
+}
